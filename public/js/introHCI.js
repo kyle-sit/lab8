e1efed4f121bb53eefb7238ad0eb8dcf2e9b6c56
Kyle Sit <ktsit@ucsd.edu>
2018-02-28 22:56:07 -0800

    thirdpartyAPI Lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -15,12 +15,37 @@ function initializePage() {
  	initRSVPForm();
 }
 
+/*$(function() {
+	$('div.box').bind('taphold', tapholdHandler);
+	function tapholdHandler(event) {
+		$(event.target).addClass('taphold');
+	}
+});*/
+
 // init jQuery gestures  
 function initGestures() {
-	// add gestures listener here
+	$(function() {
+		$('.judge-img').bind('taphold', tapholdHandler);
+
+		function tapholdHandler(event) {
+			var targetIDPrefix = event.target.id;
+			console.log("prefix: " + targetIDPrefix);
+			$('#' + targetIDPrefix +'-bio').show();
+		}
+	});
 }
 
 // init RSVP form submit listener
 function initRSVPForm() {
   // add your code here
+  $('#rsvpForm').submit(function(e) {
+  	e.preventDefault();
+  	var rsvpEmail = $('#rsvpEmail').val();
+  	$.post('addRSVP', {rsvpEmail: rsvpEmail}, postCallback);
+  });
+
+  function postCallback(res) {
+  	alert('rsvp form successful');
+  	$('rsvpEmail').val('');
+  }
 }
